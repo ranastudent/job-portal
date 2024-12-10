bf9412a64cf0237b53b95095ac1fae4b81f5781f
Rana <reduanulislam92665@gmail.com>
2024-12-11 01:21:15 +0600

    first commit

---
+++ src/router/router.jsx
@@ -0,0 +1,34 @@
+import {
+      createBrowserRouter,
+     
+    } from "react-router-dom";
+import MainLayout from "../layout/MainLayout";
+import Home from "../pages/home/Home";
+import Register from "../pages/home/Register/Register";
+import SignIn from "../pages/home/SignIn/SignIn";
+
+
+    const router = createBrowserRouter([
+      {
+        path: "/",
+        element: <MainLayout></MainLayout>,
+        errorElement: <h2>route not found</h2>,
+        children: [
+            {
+                  path:'/',
+                  element:<Home></Home>
+            },
+            {
+                  path:'register',
+                  element:<Register></Register>
+            },
+            {
+                  path:'signin',
+                  element:<SignIn></SignIn>,
+            },
+        ]
+      },
+    ]);
+
+    export default router
+    
