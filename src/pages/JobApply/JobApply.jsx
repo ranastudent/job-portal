990e1ad29c736a0a508360935c63b3a74b6712ed
Rana <reduanulislam92665@gmail.com>
2024-12-14 13:02:17 +0600

    my post jobchange

--- src/pages/JobApply/JobApply.jsx
+++ src/pages/JobApply/JobApply.jsx
@@ -24,7 +24,7 @@ const JobApply = () => {
                   github,
                   resume
             }
-            fetch('http://localhost:5000/jobs-application', {
+            fetch('http://localhost:5000/job-application', {
                   method: 'POST',
                   headers: {
                         'content-type': 'application/json'
--- src/pages/MyPostesJob/MyPostesJob.jsx
+++ src/pages/MyPostesJob/MyPostesJob.jsx
@@ -3,18 +3,44 @@ import useAuth from '../../hooks/useAuth';
 
 const MyPostesJob = () => {
       const [jobs, setJobs] = useState([])
-      const {user} = useAuth()
-      useEffect(()=>{
+      const { user } = useAuth()
+      useEffect(() => {
             fetch(`http://localhost:5000/jobs?email=${user.email}`)
-            .then(res=>res.json())
-            .then(data=>{
-                  setJobs(data)
-            })
+                  .then(res => res.json())
+                  .then(data => {
+                        setJobs(data)
+                  })
 
-      },[user.email])
+      }, [user.email])
       return (
             <div>
                   <h2>My Post Job: {jobs.length}</h2>
+                  <div className="overflow-x-auto">
+                        <table className="table table-zebra">
+                              {/* head */}
+                              <thead>
+                                    <tr>
+                                          <th></th>
+                                          <th>Job Title</th>
+                                          <th>DeadLine</th>
+                                          <th>Application Count</th>
+                                    </tr>
+                              </thead>
+                              <tbody>
+                                    {
+                                          jobs.map((job, index) => (
+                                                <tr key={index}>
+                                                      <th>{index + 1}</th>
+                                                      <td>{job.title}</td>
+                                                      <td>{job.applicationDeadline}</td>
+                                                      <td>{job.applicationCount}</td>
+                                                </tr>
+                                          ))
+                                    }
+                              </tbody>
+                        </table>
+                  </div>
+
             </div>
       );
 };
