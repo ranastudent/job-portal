051bc88bc4f0d4244868569f3d14331e13187ac7
Rana <reduanulislam92665@gmail.com>
2024-12-15 20:12:42 +0600

    axios impliment

--- src/pages/home/SignIn/SignIn.jsx
+++ src/pages/home/SignIn/SignIn.jsx
@@ -4,6 +4,7 @@ import loginLottieData from '../../../assets/lottie/login.json'
 import AuthContext from '../../../context/AuthContext/AuthContext';
 import SocialLogin from '../share/SocialLogin';
 import { useLocation, useNavigate } from 'react-router-dom';
+import axios from 'axios';
 
 const SignIn = () => {
       const {signInUser} = useContext(AuthContext)
@@ -20,8 +21,13 @@ const SignIn = () => {
             
             signInUser(email, password)
             .then(result =>{
-                  console.log('sign in',result.user)
-                  navigate(from)
+                  console.log('sign in',result.user.email)
+                  const user = {email: email}
+                  axios.post('http://localhost:5000/jwt', user)
+                  .then(res=>{
+                        console.log(res.data)
+                  })
+                  // navigate(from)
             })
             .catch(error =>{
                   console.log(error.message)
